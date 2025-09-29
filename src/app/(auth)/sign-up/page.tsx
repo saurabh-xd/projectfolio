fcc78a2a298442207e33bf0deee1a277cff8c649
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-09-30 01:06:58 +0530

    feat: implement signup page with form validation and submission

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,107 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { Form, FormControl,  FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import {  useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import z from 'zod'
+
+
+const formSchema = z.object({
+    username:  z.string().min(2).max(50),
+    email:     z.string().email(),
+    password:  z.string().min(2).max(50)
+})
+
+function Signup() {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            email: "",
+            password: ""
+        },
+    })
+
+    const router = useRouter()
+
+    async  function onSubmit(values: z.infer<typeof formSchema>){
+
+        try {
+            const res = await axios.post('/api/signup', values)
+
+            toast.success("user registered successfully")
+
+            router.replace("/")
+        } catch (error: any) {
+            toast.error("error in signup")
+            console.log(error.message);
+            
+        }
+    }
+
+  return (
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+        <div className='w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md'>
+             <div className="text-center">
+            <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+              Sign up</h1>
+             
+          </div>
+             <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter Username" {...field} />
+              </FormControl>
+             
+              <FormMessage />
+            </FormItem>
+          )}
+        /> 
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter Email" type='email' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> 
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter Password " type='password' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> 
+        <Button type="submit" className='cursor-pointer'>Sign Up</Button>
+      </form>
+    </Form>
+        </div>
+    </div>
+  
+  )
+}
+
+export default Signup
