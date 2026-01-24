ba78178ffbb47498df59e120a011d4f7b139aae8
saurabh garkoti <saurabhgarkotiii@gmail.com>
2026-01-24 12:08:18 +0530

    feat: add Vercel Analytics integration and update metadata for improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import Navbar from "@/components/layout/Navbar";
 import { Providers } from "@/components/common/providers";
 import { Poppins } from "next/font/google"
 import Footer from "@/components/layout/Footer";
+import { Analytics } from "@vercel/analytics/next";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +25,40 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: "Projectfolio",
-  description: "Show your projects",
+  metadataBase: new URL("https://projectfolio.vercel.app"),
+
+  title: {
+    default: "ProjectFolio",
+    template: "%s | ProjectFolio",
+  },
+
+  description:
+    "ProjectFolio is a social platform where developers showcase projects, discover inspiring work, and connect with builders.",
+
+  openGraph: {
+    title: "ProjectFolio – Discover & Showcase Developer Projects",
+    description:
+      "Explore real-world projects built by developers. Like, bookmark, and discover inspiring work on ProjectFolio.",
+    url: "https://projectfolio.vercel.app",
+    siteName: "ProjectFolio",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+        alt: "ProjectFolio – Developer Project Showcase",
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "ProjectFolio – Developer Project Showcase",
+    description:
+      "Showcase your projects, explore others’ work, and get inspired on ProjectFolio.",
+    images: ["/og.png"],
+  },
 };
 
 export default function RootLayout({
@@ -42,6 +76,7 @@ export default function RootLayout({
         <Navbar/>
         
         {children}
+            <Analytics />
 
         <Footer/>
 
