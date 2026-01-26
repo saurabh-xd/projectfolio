7b39156fd007e412ff86d51ca41e432f500d4ae2
saurabh garkoti <saurabhgarkotiii@gmail.com>
2026-01-26 11:43:54 +0530

    fix: middleware issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request })
+  const token = await getToken({ req: request,
+    secret: process.env.NEXTAUTH_SECRET
+   })
   const url = request.nextUrl
 
   // Redirect authenticated users away from auth pages
